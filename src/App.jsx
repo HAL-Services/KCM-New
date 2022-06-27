2850b5cae6518c7ee84b91d144e5d088b2023668
Aviral <aviral1101@gmail.com>
2022-06-27 20:00:03 +0530

    GarageCarousel updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Login from "./components/Login";
 import NavBar from "./components/NavBar";
 import HomePage from "./Pages/HomePage";
 import MarqueeElement from "./components/Marquee";
+import GarageCarousel from "./components/GarageCarousel";
 function App() {
   return (
     <div className="App">
       <HomePage />
-      <MarqueeElement/>
     </div>
   );
 }
