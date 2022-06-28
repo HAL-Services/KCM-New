ee27cc3d81c21a7eee1a26608b6c77e070b860cf
Aviral <aviral1101@gmail.com>
2022-06-28 14:48:45 +0530

    Landing Page Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import NavBar from "./components/NavBar";
 import HomePage from "./Pages/HomePage";
 import MarqueeElement from "./components/Marquee";
 import GarageCarousel from "./components/GarageCarousel";
+import LandingPage from "./Pages/LandingPage";
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <NavBar />
+      <LandingPage />
+      <MarqueeElement/>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,26 @@
+import LandingImage from "../Assets/Images/LandingImage.png";
+import "../styles/LandingPage.styles.css";
+export default function LandingPage() {
+  return (
+    <>
+      <div className="contactUs">
+        <span>Email Us -- </span>
+        KCM@gmail.com
+      </div>
+      <div
+        className="LandingPageContainer"
+        style={{
+          backgroundImage: `url(${LandingImage})`,
+        }}
+      >
+        <div className="LandingPageWrapper">
+          <h1>The German Car Expert</h1>
+          <h4>Working with brands from all over the world</h4>
+          <button>Quick Book</button>
+          <div className="LandingText">A Car Service Station in Gurugram</div>
+          <div className="LandingText1">One Stop Shop for all that your Car needs</div>
+        </div>
+      </div>
+    </>
+  );
+}
