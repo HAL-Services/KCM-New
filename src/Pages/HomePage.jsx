fa5d9330db7018f9c64db7a62fd57a992ab22ceb
Luvjeet Singh <luzag1402@gmail.com>
2022-06-28 16:57:29 +0530

    Responsive Footer

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,15 @@
-import Carousel from "../components/Carousel";
-import ExpertiseImagedata from "../OfflineAPI/ExpertiseImagesData";
+// import Carousel from "../components/Carousel";
+// import ExpertiseImagedata from "../OfflineAPI/ExpertiseImagesData";
+// import NavBar from "../components/NavBar";
+import Footer from "../components/Footer.jsx";
 export default function HomePage() {
   return (
-    <div >
-      <h1 style={{ padding: "20px" }}>Our Expertise</h1>
+    <div>
+      {/*      <h1 style={{ padding: "20px" }}>Our Expertise</h1>
       <Carousel data={ExpertiseImagedata} />
+*/}
+      {/* <NavBar /> */}
+      <Footer />
     </div>
   );
 }
