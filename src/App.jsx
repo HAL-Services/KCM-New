8d59cd77669d392aac035192730628ab17add706
Luvjeet Singh <luzag1402@gmail.com>
2022-06-30 10:29:14 +0530

    Final cards done

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,24 @@ import LandingPage from "./Pages/LandingPage";
 function App() {
   return (
     <div className="App">
-      {/* <NavBar />
+      <NavBar />
       <LandingPage />
-      <MarqueeElement/>
-      <HomePage/> */}
-      <div className="wrapper">
-        {CardData.map((ele) => {
-          return (
-            <Card
-              title={ele.title}
-              body={ele.content}
-              key={ele.id}
-              number={ele.id}
-            />
-          );
-        })}
+      <MarqueeElement />
+      <HomePage />
+      <div className="how_we_work">
+        <div className="how_we_work_title">How we Work</div>
+        <div className="wrapper">
+          {CardData.map((ele) => {
+            return (
+              <Card
+                title={ele.title}
+                body={ele.content}
+                key={ele.id}
+                number={ele.id}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
