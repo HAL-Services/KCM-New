b8bd8db3654330df6e41588635f1077cde1f91cf
Luvjeet Singh <luzag1402@gmail.com>
2022-07-01 13:49:57 +0530

    Added animation on lading page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,27 +1,55 @@
 import LandingImage from "../Assets/Images/LandingImage.png";
 import "../styles/LandingPage.styles.css";
+// Importing animation
+import { motion } from "framer-motion";
+import { landingAnim, titleAnim } from "../animation";
 export default function LandingPage() {
   return (
     <>
-      <div className="contactUs">
-        Email Us - HalServices@gmail.com 
-      </div>
-      <div
+      <div className="contactUs">Email Us - HalServices@gmail.com</div>
+      <motion.div
+        variants={landingAnim}
+        initial="hidden"
+        animate="show"
         className="LandingPageContainer"
         style={{
           backgroundImage: `url(${LandingImage})`,
         }}
       >
         <div className="LandingPageWrapper">
-          <h1>The German Car Expert</h1>
-          <h4>Working with brands from all over the world</h4>
-          <button>Quick Book</button>
-          <div className="LandingText">A Car Service Station in Gurugram</div>
-          <div className="LandingText1">
-            One Stop Shop for all that your Car needs
-          </div>
+          <span className="landing_hide">
+            <motion.h1 variants={titleAnim}>The German Car Expert</motion.h1>
+          </span>
+          <span className="landing_hide">
+            <motion.h4 variants={titleAnim}>
+              Working with brands from all over the world
+            </motion.h4>
+          </span>
+          <span className="landing_hide">
+            <motion.button
+              variants={titleAnim}
+              whileHover={{
+                backgroundColor: "hsl(243, 80%, 68%)",
+                color: "var(--default-color)",
+                transition: "0.4s",
+                cursor: "pointer",
+              }}
+            >
+              Quick Book
+            </motion.button>
+          </span>
+          <span className="landing_hide">
+            <motion.div className="LandingText" variants={titleAnim}>
+              A Car Service Station in Gurugram
+            </motion.div>
+          </span>
+          <span className="landing_hide">
+            <motion.div className="LandingText1" variants={titleAnim}>
+              One Stop Shop for all that your Car needs
+            </motion.div>
+          </span>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
--- src/animation.js
+++ src/animation.js
@@ -38,3 +38,24 @@ export const hamburgerBotAnim = {
     },
   },
 };
+
+export const landingAnim = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.75,
+      // when: "beforeChildren",
+      // staggerChildren: 0.25,
+    },
+  },
+};
+
+export const titleAnim = {
+  hidden: { y: 100 },
+  show: { y: 0, transition: { duration: 1.5, ease: "easeOut" } },
+};
