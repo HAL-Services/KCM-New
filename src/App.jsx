5424f9f90ff26b9be75c2bd1d57547297d451b2a
Aviral <aviral1101@gmail.com>
2022-08-23 04:55:29 +0530

    diff managed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
         <Route exact path="/" component={FullPage} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/about" component={AboutUs} />
       </Switch>
     </div>
   );
