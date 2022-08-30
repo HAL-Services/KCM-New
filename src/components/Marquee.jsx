e5383d5aafbb401063c6f518935073e4351982db
Aviral <aviral1101@gmail.com>
2022-08-30 16:45:19 +0530

    marquee updated

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,22 +1,9 @@
 import Marquee from "react-fast-marquee";
 import "../styles/Marquee.styles.scss";
 import MarqueeData from "../OfflineAPI/MarqueeImagesData";
-import { motion, useViewportScroll, useTransform } from "framer-motion";
-import { landingAnim, titleAnim } from "../animation";
 export default function MarqueeElement() {
   return (
-    <motion.div
-      initial={{ y: 150, opacity: 0 }}
-      whileInView={{ y: 0, opacity: 1 }}
-      transition={{
-        delay: 0,
-        ease: "linear",
-        type: "spring",
-      }}
-      viewport={{ once: false, amount: 0.1 }}
-      className="MarqueeContainer"
-      style={{ backgroundColor: "whitesmoke" }}
-    >
+    <div className="MarqueeContainer">
       <Marquee gradient={false}>
         {MarqueeData.map((imgData) => (
           <div key={imgData.id} className="MarqueeDiv">
@@ -24,6 +11,6 @@ export default function MarqueeElement() {
           </div>
         ))}
       </Marquee>
-    </motion.div>
+    </div>
   );
 }
