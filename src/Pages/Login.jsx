cb1b3f1812f7dfbc04f57481e48ecca05b0931ff
Luvjeet Singh <luzag1402@gmail.com>
2022-09-14 00:46:02 +0530

    Login signup done api done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,8 +5,9 @@ import { Link } from "react-router-dom";
 import SignUpSvg from "../Assets/Images/login.jpg";
 import { ToastContainer, toast } from "react-toastify";
 import { Context } from "../context/Context";
-import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
+import { loginUser } from "../apiCalls.js";
+
 const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -26,11 +27,10 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("http://localhost:5000/login", {
-        email: emailRef.current.value,
-        password: passwordRef.current.value,
-      });
-      const user = res.data;
+      const user = await loginUser(
+        emailRef.current.value,
+        passwordRef.current.value
+      );
       localStorage.setItem("User", JSON.stringify(user));
       dispatch({ type: "LOGIN_SUCCESS", payload: user });
     } catch (err) {
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -14,8 +14,34 @@ export const loginCall = async (userCredentials, dispatch) => {
 export const fetchTestimonialData = async () => {
   try {
     const response = await axios.get(getApiUrl("/reviews/filtered"));
-    return response.data;
+    return Promise.resolve(response.data);
   } catch (err) {
-    console.log(err.message);
+    return Promise.reject(err.response);
+  }
+};
+
+export const loginUser = async (email, password) => {
+  try {
+    const response = await axios.post(getApiUrl("/login"), {
+      email: email,
+      password: password,
+    });
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err.response);
+  }
+};
+
+export const signUpUser = async (username, email, password, mobile) => {
+  try {
+    const response = await axios.post(getApiUrl("/signup"), {
+      username: username,
+      email: email,
+      mobile: mobile,
+      password: password,
+    });
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err.response);
   }
 };
