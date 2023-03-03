fbd31e687915ae27e39ada5f0d64713cede24219
Aviral Budhani <aviral1101@gmail.com>
2023-03-03 19:15:59 +0530

    temp changes

--- src/Pages/Section2.jsx
+++ src/Pages/Section2.jsx
@@ -1,23 +1,16 @@
 import GarageCarousel from "../components/GarageCarousel";
 import WaveImage from "../Assets/Images/wave.svg";
-import "../styles/Section2.styles.css";
+import "../styles/Section2.styles.scss";
 export default function Section2() {
   return (
-    <div className="Section2PageContainer">
-      <section className="Section2PageSection">
-      <h2 className="about_title">The German Car
-            <h3 className="experience">Experts
-              <span>EXPERTS</span>
-            </h3>
-          </h2>
-        <GarageCarousel />
-      </section>
-      <img
-        className="waveImage"
-        src={WaveImage}
-        style={{ marginTop: "-0.8rem", zIndex: "-1" }}
-        alt="wave"
-      ></img>
+    <div className="section2-container">
+
+      <h2 className="about_title">Latest from
+        <h3 className="experience">Garage
+          <span>GARAGE</span>
+        </h3>
+      </h2>
+      <GarageCarousel />
     </div>
   );
 }
