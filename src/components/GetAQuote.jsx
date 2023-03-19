a5800cd6778e6f937105c1f70e5aff823053d07a
Luvjeet Singh <luzag1402@gmail.com>
2023-03-19 17:41:55 +0530

    Changes

--- src/components/GetAQuote.jsx
+++ src/components/GetAQuote.jsx
@@ -86,11 +86,13 @@ const GetAQuote = ({ close }) => {
             <label htmlFor="form-input-manufacturing">Manufacturing Year</label>
           </div>
           <div className="input-container">
-            <select>
-              <option value="disel">Disel</option>
+            <select required={true}>
+              <option hidden></option>
+              <option value="diesel">Diesel</option>
               <option value="petrol">Petrol</option>
               <option value="hybrid">Hybrid</option>
             </select>
+            <label htmlFor="form-input-manufacturing">Engine Type</label>
           </div>
           <div className="button_container">
             <button>Sumbit</button>
